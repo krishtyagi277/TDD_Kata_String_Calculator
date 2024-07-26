d3b287f1aee7594790ac8960e9f79fabc4122753
NAGARRO\krishnatyagi <krishna.tyagi@nagarro.com>
2024-07-26 17:50:25 +0530

    Split string using \n or ,(comma)

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -21,4 +21,12 @@ describe("StringCalculator", () => {
     expect(calculator.add("1,2,3")).toBe(6);
     expect(calculator.add("1,2")).toBe(3);
   });
+
+  test("should handle an unknown amount of numbers", () => {
+    expect(calculator.add("1,2,3,4")).toBe(10);
+  });
+
+  test("should handle new lines between numbers", () => {
+    expect(calculator.add("1\n2,3")).toBe(6);
+  });
 });
