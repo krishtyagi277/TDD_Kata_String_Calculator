92fff1b5893065ad2ca6e6c308da2fc5e561aa22
NAGARRO\krishnatyagi <krishna.tyagi@nagarro.com>
2024-07-26 20:10:48 +0530

    Add unit test case for negative values

--- StringCalculator.test.js
+++ StringCalculator.test.js
@@ -34,4 +34,11 @@ describe("StringCalculator", () => {
     expect(calculator.add("//;\n1;2")).toBe(3);
     expect(calculator.add("//[***]\n1***2***3")).toBe(6);
   });
+
+  test("should throw an exception for negative numbers", () => {
+    expect(() => calculator.add("1,-2,3")).toThrow("Negatives not allowed: -2");
+    expect(() => calculator.add("-1,-2,3")).toThrow(
+      "Negatives not allowed: -1, -2"
+    );
+  });
 });
